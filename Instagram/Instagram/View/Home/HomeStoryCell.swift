2b9d2879644a7fa820d9d9a8b47ef21bad37b96f
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-01-31 00:53:28 +0900

    [Add] HomeStoryCell

---
+++ Instagram/Instagram/View/Home/HomeStoryCell.swift
@@ -0,0 +1,20 @@
+//
+//  HomeStoryCell.swift
+//  Instagram
+//
+//  Created by 최한호 on 2023/01/31.
+//
+
+import SwiftUI
+
+struct HomeStoryCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeStoryCell_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeStoryCell()
+    }
+}
