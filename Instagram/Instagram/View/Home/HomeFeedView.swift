d975b3bd3ecb7af4c66ef02ab7271f2fd842c036
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-01-31 00:53:33 +0900

    [Add] HomeFeedView

---
+++ Instagram/Instagram/View/Home/HomeFeedView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeFeedView.swift
+//  Instagram
+//
+//  Created by 최한호 on 2023/01/31.
+//
+
+import SwiftUI
+
+struct HomeFeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeFeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeFeedView()
+    }
+}
