df66e1240906807277068cb8cbd1682f7ab33b26
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-01-31 00:32:20 +0900

    [Add] HomeNaviBar
    [Add] HomeNaviBar

---
+++ Instagram/Instagram/View/Home/HomeNaviBar.swift
@@ -0,0 +1,27 @@
+//
+//  HomeNaviBar.swift
+//  Instagram
+//
+//  Created by 최한호 on 2023/01/31.
+//
+
+import SwiftUI
+
+struct HomeNaviBar: View {
+    var body: some View {
+        HStack {
+            Button {
+                
+            } label: {
+                Text("Instagram")
+            }
+
+        }
+    }
+}
+
+struct HomeNaviBar_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeNaviBar()
+    }
+}
