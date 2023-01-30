8f9ebb64f210d14a1c8d5f6c1c84b538ac58b175
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-01-31 00:50:03 +0900

    [Feat] HomeNaviBar 뷰 그리기
    [Feat] HomeNaviBar 뷰 그리기

--- Instagram/Instagram/View/Home/HomeNaviBar.swift
+++ Instagram/Instagram/View/Home/HomeNaviBar.swift
@@ -10,13 +10,51 @@ import SwiftUI
 struct HomeNaviBar: View {
     var body: some View {
         HStack {
+            // MARK: - Logo Button
             Button {
                 
             } label: {
-                Text("Instagram")
+                HStack {
+                    Image("Logo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100, height: 100)
+                    
+                    Image(systemName: "chevron.down")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 10,height: 10)
+                }
+            } // Button: Logo
+            
+            Spacer()
+            
+            HStack(spacing: 20) {
+                // MARK: - Add Button
+                Button {
+                    
+                } label: {
+                    Image("Add")
+                } // Button: Add
+                
+                // MARK: - Notification Button
+                Button {
+                    
+                } label: {
+                    Image("Like")
+                }
+                
+                // MARK: - DM Button
+                Button {
+                    
+                } label: {
+                    Image("DM")
+                }
             }
 
-        }
+
+        } // HStack
+        .padding(.horizontal)
     }
 }
 
