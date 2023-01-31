2e7ba2a05dc376759b8ac3fe9ec525745918127e
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2023-01-31 10:32:49 +0900

    [Add] NotificationView / DMView
    [Add] NotificationView / DMView

---
+++ Instagram/Instagram/View/DM/DMView.swift
@@ -0,0 +1,20 @@
+//
+//  DMView.swift
+//  Instagram
+//
+//  Created by 최한호 on 2023/01/31.
+//
+
+import SwiftUI
+
+struct DMView: View {
+    var body: some View {
+        Text("DM")
+    }
+}
+
+struct DMView_Previews: PreviewProvider {
+    static var previews: some View {
+        DMView()
+    }
+}
---
+++ Instagram/Instagram/View/Home/Notification/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  Instagram
+//
+//  Created by 최한호 on 2023/01/31.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Notification")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
